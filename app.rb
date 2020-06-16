bb383210c0ddc89e2699efcf6b7684e271a607aa
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-16 02:53:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  
+  get '/name' do
+    "My name is Oswald"
+  end
+
+  get '/hometown' do
+    "My hometown is North Bergen"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Paroxysm'"
+  end
 
 end
