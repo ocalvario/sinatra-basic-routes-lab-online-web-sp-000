7a6eccfa7eb12d7324c1116a952a09d91c9d359e
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-16 02:53:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Oswald"
+  end
+
+  get '/hometown' do
+    "My hometown is North Bergen"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Paroxysm'"
+  end
+
 end
